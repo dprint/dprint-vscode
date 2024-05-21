0b0f70e9b273548bd40cedb32119f9e5c7cb9f22
David Sherret <dsherret@users.noreply.github.com>
2024-05-21 15:01:34 -0400

    fix: ensure duplicate processes don't appear on some error states and restarting (#85)

--- src/legacy/editor-service/EditorService.ts
+++ src/legacy/editor-service/EditorService.ts
@@ -1,7 +1,7 @@
 import * as vscode from "vscode";
 
 export interface EditorService {
-  kill(): void;
+  killAndDispose(): void;
   canFormat(filePath: string): Promise<boolean>;
   formatText(filePath: string, fileText: string, token: vscode.CancellationToken): Promise<string | undefined>;
 }
